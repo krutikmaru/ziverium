4c469d9ae370279dbbcd2e6d3be2fcf564548c08
Alias_Inja <alias_alex@Alias-Inja.local>
2024-07-31 01:38:29 +0530

    Adding coming soon

---
+++ app/BusinessCard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#2c282c]">
+      <h1 className="text-[#c4c4c4] text-xl tracking-tight font-medium">
+        Business Card
+      </h1>
+    </main>
+  );
+}
