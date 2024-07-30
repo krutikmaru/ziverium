1cf5f638fe1ef1ab4d0b6cdbcf320ef25ba07792
Advani786 <advani.abrar16260@gmail.com>
2024-07-31 01:51:39 +0530

    lmao

--- app/BusinessCard/page.tsx
+++ app/BusinessCard/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#2c282c]">
       <h1 className="text-[#c4c4c4] text-xl tracking-tight font-medium">
-        Business Card Abrar
+        Business Card cuming Soon
       </h1>
     </main>
   );
