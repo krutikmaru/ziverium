ef4841e572083a1d202cedc405e09ceeea615061
Alias_Alex <shaikh.injamul06905@gmail.com>
2024-08-15 16:34:12 +0530

    Mobile Overlay fix1

--- app/(pages)/nfc_center/page.tsx
+++ app/(pages)/nfc_center/page.tsx
@@ -4,6 +4,7 @@ import { Input } from "@/components/ui/input";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import Head from "next/head";
 export default function NFC_Hub() {
   const [CustName, setCustName] = useState("");
   const [phone, setPhone] = useState("");
@@ -17,6 +18,7 @@ export default function NFC_Hub() {
   const [isOverlayVisible, setOverlayVisible] = useState(false);
 
   const giveAccessButton = async () => {
+    setScanProgress("Scanning");
     setOverlayVisible(true);
     if ("NDEFReader" in window) {
       try {
@@ -69,103 +71,111 @@ export default function NFC_Hub() {
   };
 
   return (
-    <div className="text-white dark:text-white relative overflow-hidden ">
-      <div className="flex flex-col md:flex-row justify-center items-center w-screen min-h-screen relative overflow-hidden px-5 md:px-20">
-        <div className="absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-b from-transparent to-ziverium-background-dark z-20"></div>
-        <NorthernLights />
-        <div className="  w-full h-full flex flex-col items-center md:items-start justify-center relative z-20 mt-36 md:mt-0 mb-10">
-          <h1 className="scroll-m-20 text-7xl font-normal tracking-tight lg:text-8xl mb-4">
-            Welcome to NFC Hub
-          </h1>
-          <p className="text-base md:text-xl font-light text-neutral-300 mb-5 text-center md:text-left">
-            Give Access Control
-          </p>
-          <p className="text-base md:text-xl font-light text-neutral-300 mb-5 text-center md:text-left">
-            Enter Name:
-          </p>{" "}
-          <Input
-            type="text" // Use text for Client Name
-            name="ClientName"
-            value={CustName}
-            onChange={(e) => setCustName(e.target.value)} // Update state on change
-            placeholder="Enter Full Name:"
-            style={{
-              width: "350px",
-              height: "45px",
-              padding: "12px 20px",
-              margin: "8px 0",
-              boxSizing: "border-box",
-              border: "2px solid #ccc",
-              borderRadius: "4px",
-              fontSize: "21px",
-              background: "transparent",
-              fontFamily: "Arial, sans-serif",
-              transition: "border-color 0.3s ease, box-shadow 0.3s ease",
-            }}
-          />
-          <p className="text-base md:text-xl font-light text-neutral-300 mb-5 text-center md:text-left">
-            Enter Phone Number:
-          </p>
-          <Input
-            type="number" // Use text for Phone number
-            name="Phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)} // Update state on change
-            placeholder="Enter Phone  Number:"
-            style={{
-              width: "350px",
-              height: "45px",
-              padding: "12px 20px",
-              margin: "8px 0",
-              boxSizing: "border-box",
-              border: "2px solid #ccc",
-              borderRadius: "4px",
-              fontSize: "21px",
-              background: "transparent",
-              fontFamily: "Arial, sans-serif",
-              transition: "border-color 0.3s ease, box-shadow 0.3s ease",
-            }}
-          />
-        </div>
-        <div className=" w-full h-full relative overflow-visible bg-transparent">
-          <button
-            className="bg-ziverium-blue text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 "
-            onClick={giveAccessButton}
-          >
-            <span>Give Access</span>
-          </button>
-          {isOverlayVisible && (
-            <div
-              className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
-              onClick={handleOverlayClick}
+    <>
+      {" "}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className="text-white dark:text-white relative overflow-hidden ">
+        <div className="flex flex-col md:flex-row justify-center items-center w-screen min-h-screen relative overflow-hidden px-5 md:px-20">
+          <div className="absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-b from-transparent to-ziverium-background-dark z-20"></div>
+          <NorthernLights />
+          <div className="  w-full h-full flex flex-col items-center md:items-start justify-center relative z-20 mt-36 md:mt-0 mb-10">
+            <h1 className="scroll-m-20 text-7xl font-normal tracking-tight lg:text-8xl mb-4">
+              Welcome to NFC Hub
+            </h1>
+            <p className="text-base md:text-xl font-light text-neutral-300 mb-5 text-center md:text-left">
+              Give Access Control
+            </p>
+            <p>{scanProgress}</p>
+            <p className="text-base md:text-xl font-light text-neutral-300 mb-5 text-center md:text-left">
+              Enter Name:
+            </p>
+            <Input
+              type="text" // Use text for Client Name
+              name="ClientName"
+              value={CustName}
+              onChange={(e) => setCustName(e.target.value)} // Update state on change
+              placeholder="Enter Full Name:"
+              style={{
+                width: "350px",
+                height: "45px",
+                padding: "12px 20px",
+                margin: "8px 0",
+                boxSizing: "border-box",
+                border: "2px solid #ccc",
+                borderRadius: "4px",
+                fontSize: "21px",
+                background: "transparent",
+                fontFamily: "Arial, sans-serif",
+                transition: "border-color 0.3s ease, box-shadow 0.3s ease",
+              }}
+            />
+            <p className="text-base md:text-xl font-light text-neutral-300 mb-5 text-center md:text-left">
+              Enter Phone Number:
+            </p>
+            <Input
+              type="number" // Use text for Phone number
+              name="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)} // Update state on change
+              placeholder="Enter Phone  Number:"
+              style={{
+                width: "350px",
+                height: "45px",
+                padding: "12px 20px",
+                margin: "8px 0",
+                boxSizing: "border-box",
+                border: "2px solid #ccc",
+                borderRadius: "4px",
+                fontSize: "21px",
+                background: "transparent",
+                fontFamily: "Arial, sans-serif",
+                transition: "border-color 0.3s ease, box-shadow 0.3s ease",
+              }}
+            />
+            <button
+              className="bg-ziverium-blue text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 "
+              onClick={giveAccessButton}
+              onTouchStart={giveAccessButton} /* Added touch event */
             >
-              {status ? (
-                <div className="text-white text-2xl">
-                  <p>Full Name: </p>
-                  {CustName}
-                  <p>Cust ID: </p>
-                  {custID}
-                  <p>Phone: </p>
-                  {phone}
-                  <button className="bg-ziverium-blue text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 ">
-                    <span>Confirm Access</span>
-                  </button>
-                </div>
-              ) : (
-                <div className="text-white text-2xl">
-                  Tap Your Access Card to Give Access
-                </div>
-              )}
-            </div>
-          )}
-          <button className="bg-transparent text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 ">
-            <span></span>
-          </button>
-          <button className="bg-ziverium-blue text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 ">
-            <span>Access Control List</span>
-          </button>
+              <span>Give Access</span>
+            </button>
+          </div>
+          <div className=" w-full h-full relative overflow-visible bg-transparent">
+            {isOverlayVisible && (
+              <div
+                className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+                onClick={handleOverlayClick}
+              >
+                {status ? (
+                  <div className="text-white text-2xl">
+                    <p>Full Name: </p>
+                    {CustName}
+                    <p>Cust ID: </p>
+                    {custID}
+                    <p>Phone: </p>
+                    {phone}
+                    <button className="bg-ziverium-blue text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 ">
+                      <span>Confirm Access</span>
+                    </button>
+                  </div>
+                ) : (
+                  <div className="text-white text-2xl">
+                    Tap Your Access Card to Give Access
+                  </div>
+                )}
+              </div>
+            )}
+            <button className="bg-transparent text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 ">
+              <span></span>
+            </button>
+            <button className="bg-ziverium-blue text-[#141414] py-2 px-5 font-semibold rounded-full flex items-center space-x-2 ">
+              <span>Access Control List</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
